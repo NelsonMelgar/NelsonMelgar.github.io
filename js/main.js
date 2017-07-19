9429354ea9687165222118c555b76fdbcdde579c
Nelson Melgar <nelsonmelgar@Sarahs-MacBook-Air-2.local>
2017-07-20 09:01:50 +1000

    first save

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*$('button').on('click' function() {
+	$('p').html('you clicked button');
+});*/
+
+alert("are you working?");
+
+/*$('.declare').on('click', function () {
+	$('.square').hide();
+});*/
+
+
+
+
+
+
+/*var wishData = [];
+
+//FIREBASE//
+
+$(".button").on("click", function (e){
+	e.preventDefault();
+	wishData = $("#wish").val();
+
+
+
+
+	database.ref("#wish").push(wishData);
+
+});
+
